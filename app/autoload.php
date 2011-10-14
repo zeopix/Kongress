cafa9fd211424ae2b210121c2154723453a1f652
Ivan Ruiz <ivan.ruiz.delatorre@gmail.com>
2011-10-14 23:48:49 +0200

    vendor/bundles add to project!

--- app/autoload.php
+++ app/autoload.php
@@ -15,7 +15,6 @@ $loader->registerNamespaces(array(
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
     'FOS'              => __DIR__.'/../vendor/bundles',
-    'Etcpasswd'        => __DIR__.'/../vendor/bundles',
     'Buzz'             => __DIR__.'/../vendor/buzz/lib',
 ));
 $loader->registerPrefixes(array(
